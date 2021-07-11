f1f08650ed897e34565ad9157a83c7b1790cb9ca
NasserAlghamdi-tech <85634816+NasserAlghamdi-tech@users.noreply.github.com>
2021-07-11 17:11:22 +0300

    Add files via upload

---
+++ base.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2021 at 04:02 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robots_arm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `base`
+--
+
+CREATE TABLE `base` (
+  `Forword` varchar(20) NOT NULL,
+  `BackWord` varchar(20) NOT NULL,
+  `RightSide` varchar(20) NOT NULL,
+  `LeftSide` varchar(20) NOT NULL,
+  `Stop` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
